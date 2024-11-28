299787d3a04f333f2cd35a5b4640a7a1e67ce1a9
KJ4Web <realmr.kj@gmail.com>
2024-11-29 02:37:15 +0600

    Added a solution for the problem of flatten.

---
+++ Lab Report-2/Solutions/flatten.py
@@ -0,0 +1,8 @@
+import numpy as np
+data = np.array([
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 
+    [[10, 11, 12], [13, 14, 15], [16, 17, 18]] 
+])
+flattened_data = data.flatten()
+print("Flattened data:")
+print(flattened_data)
