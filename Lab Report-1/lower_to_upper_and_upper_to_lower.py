1e25cc07c662b154b8cb9412f127b21744108835
KJ4Web <realmr.kj@gmail.com>
2024-12-02 19:40:01 +0600

    Added the solution for the problem of Convert the String into Lowercase to Uppercase and Uppercase to Lowercase.

---
+++ Lab Report-1/lower_to_upper_and_upper_to_lower.py
@@ -0,0 +1,7 @@
+a = input("Enter a lowercase string: ")
+a = a.upper()
+print("String in uppercase is:", a)
+
+b = input("Enter a uppercase string: ")
+b = b.lower()
+print("String in lowercase is:", b)
