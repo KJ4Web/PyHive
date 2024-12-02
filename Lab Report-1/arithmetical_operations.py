265bb2a96ca1e24be74fc9e1b8ad48cc75320cce
KJ4Web <realmr.kj@gmail.com>
2024-12-02 12:23:09 +0600

    Added the solution for the problem of Arithmetical Operations.

---
+++ Lab Report-1/arithmetical_operations.py
@@ -0,0 +1,10 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print("The sum of 2 numbers: ",sum)
+print("The sub of 2 numbers: ",sub)
+print("The mul of 2 numbers: ",mul)
+print("The div of 2 numbers: ",div)
